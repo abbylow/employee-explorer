be21cedc91601cad9604420e3510138e0ff13ce2
Abby Low <yu950713@gmail.com>
2020-02-04 11:19:10 +0800

    home page

---
+++ src/containers/Home.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import '../App.css';
+import { Typography, Input, IconButton } from '@material-ui/core';
+import { Magnify } from 'mdi-material-ui';
+import { searchEmployee } from '../services/getEmployee';
+
+export default class Home extends Component {
+
+  state = { searchText: '' };
+
+  updateSearchText = (e) => {
+    this.setState({ searchText: e.target.value });
+  }
+
+  handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      this.handleSearch();
+    }
+  }
+
+  handleSearch = () => {
+    searchEmployee(this.state.searchText);
+  }
+
+  render() {
+    const { searchText } = this.state;
+
+    return (
+      <div className="home" >
+        <div className='title'>
+          <Typography variant='h2' align='center'>Employee Explorer</Typography>
+        </div>
+        <div className='search-area'>
+          <div className='search-bar'>
+            <Input
+              placeholder="Enter employee name "
+              disableUnderline
+              value={searchText}
+              onChange={this.updateSearchText}
+              onKeyDown={this.handleKeyDown}
+              className='search-input'
+              fullWidth
+            />
+            <IconButton onClick={this.handleSearch}>
+              <Magnify className='search-icon' />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
---
+++ src/services/getEmployee.js
@@ -0,0 +1,4 @@
+export function searchEmployee(name){
+  console.log('search for this employee ', name)
+
+}
